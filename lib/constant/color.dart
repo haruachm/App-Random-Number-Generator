4d267ec54960e23c2b25c4663491aa0861482700
Haruachm <horang_e7@naver.com>
2023-02-14 17:54:52 +0900

    design : Change color and number matched designed image

---
+++ lib/constant/color.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color primary_color = Color(0xff2d2d33);
+const Color red_color = Color(0xffae4955);
+const Color blue_color = Color(0xff549fbf);
+const Color green_color = Color(0xff26FF00);
+const Color white_color = Color(0xffffffff);
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: prefer_const_constructors
+import 'package:app_random_number_generaotr/constant/color.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,39 +13,77 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: primary_color,
       body: SafeArea(
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'RANDOM NUMBER MAKER',
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.android_sharp),
-                  color: Colors.green,
-                )
-              ],
-            ),
-            Expanded(
-                child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('123'),
-                Text('123'),
-                Text('123'),
-              ],
-            )),
-            SizedBox(
-              width: double.infinity,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('랜덤숫자 생성'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    ' ⚡ NUMBER MAKER',
+                    style: TextStyle(
+                      color: white_color,
+                      fontSize: 25,
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.android_sharp,
+                      color: green_color,
+                    ),
+                  )
+                ],
               ),
-            )
-          ],
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    1234,
+                    5678,
+                    9012,
+                    3456,
+                  ]
+                      .asMap()
+                      .entries
+                      .map(
+                        (x) => Padding(
+                          padding: EdgeInsets.only(bottom: x.key == 1 ? 0 : 30),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: x.value
+                                .toString()
+                                .split('')
+                                .map((x) => Image.asset(
+                                      'asset/img/$x.png',
+                                      width: 40,
+                                      height: 60,
+                                    ))
+                                .toList(),
+                          ),
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+              SizedBox(
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: red_color,
+                  ),
+                  onPressed: () {},
+                  child: Text('랜덤숫자 생성'),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
