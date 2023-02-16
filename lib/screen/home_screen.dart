3b1adce26513f063eed882bc7dcd28f9913d9654
Haruachm <horang_e7@naver.com>
2023-02-16 13:57:52 +0900

    refactor: Optimize code to separate class

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -25,76 +25,111 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
           child: Column(
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    ' ⚡ NUMBER MAKER',
-                    style: TextStyle(
-                      color: white_color,
-                      fontSize: 25,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.android_sharp,
-                      color: green_color,
-                    ),
-                  )
-                ],
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: randomNum
-                      .asMap()
-                      .entries
-                      .map(
-                        (x) => Padding(
-                          padding: EdgeInsets.only(bottom: x.key == 3 ? 0 : 10),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: x.value
-                                .toString()
-                                .split('')
-                                .map((x) => Image.asset(
-                                      'asset/img/$x.png',
-                                      width: 40,
-                                      height: 60,
-                                    ))
-                                .toList(),
-                          ),
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    primary: red_color,
-                  ),
-                  onPressed: () {
-                    final random = Random();
-
-                    final Set<int> newNum = {};
-                    while (newNum.length != 5) {
-                      final number = random.nextInt(1000000);
-                      newNum.add(number);
-
-                      setState(() {
-                        randomNum = newNum.toList();
-                      });
-                    }
-                  },
-                  child: Text('랜덤숫자 생성'),
-                ),
-              )
+              _Hearder(),
+              _Body(randomNum: randomNum),
+              _Footer(onPressed: onRandomNumberGenerator),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  void onRandomNumberGenerator() {
+    final random = Random();
+
+    final Set<int> newNum = {};
+    while (newNum.length != 5) {
+      final number = random.nextInt(1000000);
+      newNum.add(number);
+
+      setState(() {
+        randomNum = newNum.toList();
+      });
+    }
+  }
+}
+
+class _Hearder extends StatelessWidget {
+  const _Hearder({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          ' ⚡ NUMBER MAKER',
+          style: TextStyle(
+            color: white_color,
+            fontSize: 25,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.android_sharp,
+            color: green_color,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final List<int> randomNum;
+
+  const _Body({required this.randomNum, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: randomNum
+            .asMap()
+            .entries
+            .map(
+              (x) => Padding(
+                padding: EdgeInsets.only(bottom: x.key == 3 ? 0 : 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: x.value
+                      .toString()
+                      .split('')
+                      .map((x) => Image.asset(
+                            'asset/img/$x.png',
+                            width: 40,
+                            height: 60,
+                          ))
+                      .toList(),
+                ),
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final VoidCallback onPressed;
+  const _Footer({
+    required this.onPressed,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          primary: red_color,
+        ),
+        onPressed: onPressed,
+        child: Text('랜덤숫자 생성'),
       ),
     );
   }
