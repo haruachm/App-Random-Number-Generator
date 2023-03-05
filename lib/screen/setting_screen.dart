d3912ab0f2420970d30f89f586305f2ad916caf0
haruachm <horang_e7@naver.com>
2023-03-05 17:48:53 +0900

    refactor : Recreate code useful for state management

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -22,46 +22,90 @@ class _SettingScreenState extends State<SettingScreen> {
           ),
           child: Column(
             children: [
-              Expanded(
-                child: Row(
-                  children: maxNum
-                      .toInt()
-                      .toString()
-                      .split('')
-                      .map(
-                        (x) => Image.asset(
-                          'asset/img/$x.png',
-                          width: 50.0,
-                          height: 80.0,
-                        ),
-                      )
-                      .toList(),
-                ),
-              ),
-              Slider(
-                value: maxNum,
-                min: 10000,
-                max: 1000000,
-                onChanged: (double val) {
-                  setState(() {
-                    maxNum = val;
-                  });
-                },
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(primary: red_color),
-                  onPressed: () {
-                    Navigator.of(context).pop(maxNum.toInt());
-                  },
-                  child: Text('저장하기'),
-                ),
+              _Body(maxNum: maxNum),
+              _Footer(
+                maxNum: maxNum,
+                onSliderChanged: onSliderChanged,
+                onButtonPressed: onButtonPressed,
               ),
             ],
           ),
         ),
       ),
+    );
+  }
+
+  void onSliderChanged(double val) {
+    //생성
+    setState(() {
+      maxNum = val;
+    });
+  }
+
+  void onButtonPressed() {
+    Navigator.of(context).pop(maxNum.toInt());
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNum;
+  const _Body({
+    required this.maxNum,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(
+        children: maxNum
+            .toInt()
+            .toString()
+            .split('')
+            .map(
+              (x) => Image.asset(
+                'asset/img/$x.png',
+                width: 50.0,
+                height: 80.0,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNum; //외부로부터 변수 받아옴
+  final ValueChanged<double>? onSliderChanged; //Silder onChanged 상태관리
+  final VoidCallback onButtonPressed;
+
+  const _Footer({
+    required this.maxNum,
+    required this.onSliderChanged,
+    required this.onButtonPressed,
+    Key? key,
+  }) : super(key: key); //생성자 선언
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Slider(
+            value: maxNum,
+            min: 10000,
+            max: 1000000,
+            onChanged: onSliderChanged //변경
+            ),
+        SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(primary: red_color),
+            onPressed: onButtonPressed,
+            child: Text('저장하기'),
+          ),
+        )
+      ],
     );
   }
 }
